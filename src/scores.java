882834cdbf2e1da392d2515ecdac555b5496ebae
samgr78 <samuel.galliani-royer@edu.esiee-it.fr>
2025-01-21 15:31:14 +0100

    add function showScores

---
+++ src/scores.java
@@ -0,0 +1,10 @@
+public class scores {
+
+    public static void showScores(String[] scores) {
+        String[] pseudoScores = new String[22];
+        pseudoScores[0] = "caca";
+        for (byte i=0; i<=11; i++){
+
+        }
+    }
+}
