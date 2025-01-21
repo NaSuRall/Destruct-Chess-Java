1b7af019df8fb28b8c3d4ee17547818a507959db
NaKyoo <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2025-01-21 10:01:17 +0100

    reorganise files

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,24 @@
 public class Board {
+    public static char[][] CreateBoard (int rows, int cols) {
+        char[][] board = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        return board;
+    }
 
+    public static void showBoard(char[][] board) {
+        System.out.println(" ");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+                if(j < board[i].length - 1){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -13,32 +13,9 @@ public class Game {
         System.out.println(FirstPlayer + "'s turn");
 
         char[][] board = new char[10][11];
-        showBoard(board);
+        Board.showBoard(board);
 
 
     }
 
-
-    public static char[][] CreateBoard (int rows, int cols) {
-        char[][] board = new char[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                board[i][j] = ' ';
-            }
-        }
-        return board;
-    }
-
-    public static void showBoard(char[][] board) {
-        System.out.println(" ");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-                if(j < board[i].length - 1){
-                    System.out.print(" ");
-                }
-            }
-        System.out.println();
-        }
-    }
 }
