71d927c35bd5942d6a9a9480111fc60eb15a326f
NaKyoo <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2025-01-21 10:26:16 +0100

    added player position

--- src/Game.java
+++ src/Game.java
@@ -4,11 +4,20 @@ public class Game {
 
     public static void main (String[] args) {
 
-        // Attribute name for players
+        // Attribute name for players and symbols
 
         String Player1 = "Player1";
         String Player2 = "Player2";
+        char player1Symbol = '1';
+        char player2Symbol = '2';
         String[] players = {Player1, Player2};
+
+        // Create the board
+        char[][] board = new char[10][11];
+
+        // Place the players
+        Player.placePlayer(board, player1Symbol, 4, 5);
+        Player.placePlayer(board, player2Symbol, 5, 5);
 
         // Randomise player turn at the game start
         Random random = new Random();
@@ -16,7 +25,6 @@ public class Game {
         System.out.println(FirstPlayer + "'s turn");
 
         // Show the board
-        char[][] board = new char[10][11];
         Board.showBoard(board);
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,8 @@
 public class Player {
+
+    // Place a player on the board at a specific position
+    public static void placePlayer(char[][] board, char symbol, int row, int col) {
+        board[row][col] = ' '; // Vide la case (efface son contenu)
+        board[row][col] = symbol; // Place le joueur dans la case
+    }
 }
