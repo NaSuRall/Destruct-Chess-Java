3a7fb8a2df6927a74b52958e07ffa8387b3aa283
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-01-21 11:43:35 +0100

    Add Class PlayerManager for function requestPlayerName

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PlayerManager.requestPlayerName();
+
+
     }
+
+
 }
---
+++ src/PlayerManager.java
@@ -0,0 +1,30 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PlayerManager{
+    public static void requestPlayerName(){
+        Scanner scanner = new Scanner(System.in);
+        String[] playerNames = new String[2];
+
+        for (byte i = 0; i < 2; i++) {
+            System.out.println("Entrez le nom du joueur " + (i + 1) + " :");
+            String userName = scanner.nextLine();
+
+            while (userName.length() < 2 || userName.length() > 10 || (userName.equals(playerNames[0]))) {
+                if (userName.length() < 2 || userName.length() > 10) {
+                    System.out.println("Le pseudo doit contenir entre 2 et 10 caractères. Réessayez :");
+                } else {
+                    System.out.println("Le pseudo est déjà pris. Réessayez :");
+                }
+                userName = scanner.nextLine();
+            }
+            playerNames[i] = userName;
+            System.out.println("Joueur " + (i + 1) + " est maintenant : " + playerNames[i]);
+        }
+
+        scanner.close();
+    }
+
+}
