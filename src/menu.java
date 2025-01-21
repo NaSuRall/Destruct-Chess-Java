e981fbfdbc6cbfb85fe8e16f644708cdf1f62e95
samgr78 <samuel.galliani-royer@edu.esiee-it.fr>
2025-01-21 13:19:26 +0100

    Create Branch Dev to merge project

--- src/menu.java
+++ src/menu.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class menu {
+public class Menu {
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
-        int choix;
+        byte choix;
 
         while (true) {
             System.out.println("To choose an option, tap a number ");
@@ -12,7 +12,7 @@ public class menu {
             System.out.println("3. Exit game");
 
             try {
-                choix = Integer.parseInt(scanner.nextLine());
+                choix = Byte.parseByte(scanner.nextLine());
                 switch (choix) {
                     case 1:
                         System.out.println("The game will begin");
@@ -20,7 +20,11 @@ public class menu {
                         break;
 
                     case 2:
-                        rules();
+                        System.out.println("Rules:");
+                        System.out.println("During his turn a player can move his pawn one space (vertically or horizontally),");
+                        System.out.println("then he destroys a square on the board.");
+                        System.out.println("The last player who can still move wins.");
+                        System.out.println("The winner wins 5 points and the loser loses 2 points.");
                         break;
 
                     case 3:
@@ -34,28 +38,6 @@ public class menu {
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Error: Please enter a valid number.");
-            }
-        }
-    }
-
-    public static void rules() {
-        System.out.println("Rules:");
-        System.out.println("During his turn a player can move his pawn one space (vertically or horizontally),");
-        System.out.println("then he destroys a square on the board.");
-        System.out.println("The last player who can still move wins.");
-        System.out.println("The winner wins 5 points and the loser loses 2 points.");
-        System.out.println("Tap 'r' to return to menu.");
-
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Your choice: ");
-            String choix = scanner.nextLine();
-
-            if (choix.equalsIgnoreCase("r")) { // Utiliser .equalsIgnoreCase() pour comparer des chaînes
-                menu(); // Appeler la méthode menu()
-                break;  // Sortir de la boucle après le retour au menu
-            } else {
-                System.out.println("Invalid input. Please type 'r' to return to the menu.");
             }
         }
     }
