4f9bef70d0333ed912d753e3cbdb7832e0262fbe
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-01-22 10:22:20 +0100

    Add function score player

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Menu.menu();
+        //Menu.menu();
+
+
     }
 }
